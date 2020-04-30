31b5e24410d33969c83b11921a7f07215d654576
julian.rojas@mvm.com.co <julian.rojas@mvm.com.co>
2020-04-30 01:51:18 -0500

    Add project files.

---
+++ Skinet.API/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Skinet.Core.Entities;
+using Skinet.Infrastructure.Data;
+
+namespace Skinet.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            var a = new Product();
+            var result = await _context.Products.ToListAsync();
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            return Ok(product);
+        }
+    }
+}
