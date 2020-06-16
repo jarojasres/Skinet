50ce89891771a2da274ebba62e2de8c4cec9aba4
julian.rojas@mvm.com.co <julian.rojas@mvm.com.co>
2020-06-16 14:36:07 -0500

    Add loading interceptor and home component

--- Skinet.Client/src/app/shop/product-details/product-details.component.ts
+++ Skinet.Client/src/app/shop/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IProduct } from 'src/app/shared/models/product';
 import { ShopService } from '../shop.service';
 import { ActivatedRoute } from '@angular/router';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-product-details',
@@ -12,7 +13,12 @@ export class ProductDetailsComponent implements OnInit {
   product: IProduct;
 
 
-  constructor(private shopService: ShopService, private activateRoute: ActivatedRoute) { }
+  constructor(private shopService: ShopService,
+              private activateRoute: ActivatedRoute,
+              private bcService: BreadcrumbService) {
+
+                this.bcService.set('@productDetails', '');
+   }
 
   ngOnInit(): void {
     this.loadProduct();
@@ -23,6 +29,7 @@ export class ProductDetailsComponent implements OnInit {
     const id = parseInt(this.activateRoute.snapshot.paramMap.get('id'));
     this.shopService.getProduct(id).subscribe( product => {
       this.product = product;
+      this.bcService.set('@productDetails', product.name);
     }, error => {
       console.log(error);
     });
